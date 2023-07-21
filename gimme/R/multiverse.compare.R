c6a210066018e22ba40d16590f045fc8843a48ce
Bjoern Siepe <bjoernsiepe@gmail.com>
2023-07-21 15:46:37 +0200

    add group, subgroup & individual comparison fns

--- gimme/R/multiverse.compare.R
+++ gimme/R/multiverse.compare.R
@@ -1,23 +1,38 @@
-multiverse.compare <- function(l_out,
+multiverse.compare <- function(l_res,
                                ref_model
 ){
   
   
   #---- Compare Group-Level
-  
+  comp_group <- multiverse.compare.group(
+    l_res = l_res, ref_model = ref_model
+  )
   
   #---- Compare Subgroup-Level
-  
+  comp_subgroup <- multiverse.compare.subgroup(
+    l_res = l_res, ref_model = ref_model
+  )  
   
   #---- Compare Individual Solutions
-  
+  comp_ind <- multiverse.compare.individual(
+    l_res = l_res, ref_model = ref_model
+  )   
   
   #--- Output
+  # Idea: tibble where each row is one specification
+  # individual results can be stored in dataframe inside tibble
+  comp_res <- dplyr::bind_cols(comp_group, comp_subgroup, comp_ind)
   
+  
+  # save condition for each specification 
+  conds <- do.call(rbind, lapply(l_res, `[[`, "conds"))
+  comp_res <- cbind(comp_res, conds)
+  
+  return(comp_res)
 }
 
 
-multiverse.compare.group <- function(l_out,
+multiverse.compare.group <- function(l_res,
                                      ref_model){
   
   # reference model info
@@ -27,64 +42,149 @@ multiverse.compare.group <- function(l_out,
   ref_groupedge <- ifelse(ref_model$path_counts == n_ind, 1, 0)
   
   # Find group effects adjacency matrix
-  l_ref_diff <- lapply(l_out, function(x){
+  l_ref_diff <- lapply(l_res, function(x){
     tmp_groupedge <- ifelse(x$path_counts == n_ind, 1, 0)
     diff_groupedge <- ref_groupedge - tmp_groupedge
+    return(diff_groupedge)
   }
   )
- 
+  
+  # Count occurrence of each group effect
+  ## list of adjacency matrices
+  l_adjacency<- lapply(l_res, function(x){
+    tmp_groupedge <- ifelse(x$path_counts == n_ind, 1, 0)
+    return(tmp_groupedge)
+  }
+  )
+
+  
+  #--- Output
+  l_out <- tibble(
+    n_ind = n_ind,
+    ref_diff_g = l_ref_diff, 
+    adjacency_g = l_adjacency
+  ) %>% 
+    dplyr::mutate(g_ref_diff = as.list(ref_diff_g)) %>% 
+    tidyr::unnest(n_ind)
+  
+  
 }
 
 
-multiverse.compare.subgroup <- function(l_out, 
+multiverse.compare.subgroup <- function(l_res, 
                                         ref_model){
   
   # reference model info
   n_ind <- length(ref_model$data)
   ref_sim_matrix <- ref_model$sim_matrix
   
-  # Number of subgroups
+  #--- Number of subgroups
+  l_n_sub <- lapply(l_res, function(x){
+    return(length(x$path_counts_sub))
+  }
+  )
   
-  # Size of subgroups
+  ## Summary statistics
+  # min_n_sub <- min(v_n_sub)
+  # max_n_sub <- max(v_n_sub)
+  # mean_n_sub <- max(v_n_sub)
+  # med_n_sub <- stats::median(v_n_sub)
+  # sd_n_sub <- sd(v_n_sub)
   
-  # Subgroup membership
-  # But which count matrix do we need here?
+  #--- Size of subgroups
+  l_size_sub <- lapply(l_res, function(x){
+    return(table(x$fit$sub_membership))
+  })
   
+  #--- Subgroup similarity matrix & modularity
+  ## Use similarity matrix of each specification
+  l_pert <- lapply(l_res, function(x){
+    l_calc <- list()
     # can use VI and ARI from perturbR package here
-    vi.dist(ref_sim_matrix, x)
-    arandi(ref_sim_matrix, x)
+    l_calc$vi <- vi.dist(ref_sim_matrix, x$sim_matrix)
+    l_calc$ari <- arandi(ref_sim_matrix, x$sim_matrix)
+    l_calc$modularity <- as.numeric(x$fit$modularity[1])   
+    return(l_calc)
+  })
+
+  #--- Subgroup edges
+  ## Loop over subgroups
+  l_out <- tibble(
+    n_sub_g = l_n_sub,
+    size_sub_s = l_size_sub,
+    vi_s = l_pert$vi,
+    ari_s = l_pert$ari,
+    modularity_s = l_pert$modularity
+  ) 
     
-  # Subgroup edges
-  
-    
+  return(l_out)
 }
 
 
 
-multiverse.compare.individual <- function(l_out){
+multiverse.compare.individual <- function(l_res,
+                                          ref_model){
   
-  # reference model info
+  #--- Reference model 
   n_ind <- length(ref_model$data)
   
-  ## estimates
+  ## Estimates
   ref_path_est_mats <- ref_model$path_est_mats
   
-  ## adjacency matrix
+  ## Adjacency matrix
   ref_adj_mats <- lapply(ref_path_est_mats, function(x){
     ifelse(x != 0, 1, 0)
   })
   
+  ## Fit indices
+  fit_ind_names <- c("chisq", "df", "npar", "pvalue", "rmsea", "srmr",
+                     "nnfi", "cfi", "bic", "aic", "logl")
+  ref_fit_ind <- ref_model$fit[names(ref_model$fit) %in% fit_ind_names]
   
-  # Loop over models and individuals
-  l_ind <- list()
-  for(i in 1:n_ind){
-    l_ind[[i]] <- list()
-    
-  }
-  
-  # Aggregate over individuals
+  ## Centrality
+  ref_outstrength <- lapply(ref_path_est_mats, function(x){
+    colSums(abs(x))
+  })
+
   
   
+  #--- Compare Edges
+  l_diff_adj <- lapply(l_res, function(x){
+    ## adjacency matrices
+    tmp_adj_mats <- lapply(x$path_est_mats, function(y){
+      ifelse(y != 0, 1, 0)
+    })
+    Map('-', ref_adj_mats, tmp_adj_mats)
+  })
+  ## path estimates
+  l_diff_ests <- lapply(l_res, function(x){
+    Map('-', ref_path_est_mats, x$path_est_mats)
+  })
+
+  #--- Compare Fits
+  l_diff_fit <- lapply(l_res, function(x){
+    return(ref_fit_ind - x$fit[names(x$fit) %in% fit_ind_names])
+  })
+  
+  #--- Compare centrality
+  l_diff_cent <- lapply(l_res, function(x){
+    tmp_outstrength <- lapply(x$path_est_mats, function(y){
+      colSums(abs(y))
+    })
+    Map('-', ref_outstrength, tmp_outstrength)
+  })
+  
+  #--- Aggregate
+  
+  
+  l_out <- tibble(
+    n_ind_test = n_ind,
+    l_diff_adj_s = l_diff_adj,
+    l_diff_ests_s = l_diff_ests,
+    l_diff_fit_s = l_diff_fit,
+    l_diff_cent_s = l_diff_cent
+  )
+  return(l_out)
 }
 
 
@@ -134,4 +234,22 @@ arandi <-
     }
   }
 
+
+
+# Matrix list summary -----------------------------------------------------
+matrix_summary <- function(l_matrix) {
+  # Calculate mean, median, sd, min, and max for each matrix element across the list
+  arr_list <- simplify2array(l_matrix)
+  
+  a_mean <- apply(arr_list, 1:2, mean)
+  a_median <- apply(arr_list, 1:2, stats::median)
+  a_min <- apply(arr_list, 1:2, min)
+  a_max <- apply(arr_list, 1:2, max)
+  a_sd <- apply(arr_list, 1:2, sd)
+  
+  # Combine the results into a data frame
+  summary_df <- as.data.frame(summary_list)
+  
+  return(summary_df)
+}
 
